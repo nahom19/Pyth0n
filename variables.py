9bef5f4500a9d231c6701682c4bbb0eab838d261
nahom19 <50850533+nahom19@users.noreply.github.com>
2019-06-17 19:41:39 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,95 @@
+print('nahom gebreamlak')       # display your name
+print("""pyhton's the best""" )
+print("""The cat said "meow" """)
+width = 10
+length = 5
+
+print(width)
+print(length)
+
+# This program demonstrates a variable.
+
+room = 503
+print('I am stayign in room number..')
+print(room)
+
+# cr8 2 variables: top_speed and distance
+
+top_speed = 160
+distance = 300
+
+# diplay the values refrenced by the variables
+print('The top speed is')
+print(top_speed)
+print('The distance travelled is')
+print(distance)
+
+# Uefa Champions League
+
+Liverpool = 2
+Tottenham = 0
+
+# Display the winner pf the UCL final with the score
+print('Liverpool scored...')
+print(Liverpool)
+print('Tottenham scored...')
+print(Tottenham)
+
+
+
+#This prgram demonstrates a variable.
+room = 503
+print('I am staying in room number', room)
+
+
+# This program demonstrates variable reassignment.
+# Assign a valur to the dollars variable.
+
+dollars = 2.55
+print('I have', dollars, 'in my account.')
+
+# reassign dollars so it refrences
+# a different value.
+
+dollar =  99.63
+print('But now I have', dollars, 'in my TD bank account!' )
+
+# Create variables to refrene two strings.
+first_name = 'Kirbel'
+last_name = 'Gebreamlak'
+
+
+
+# Get the user's first name.
+first_name = input('Enter your first name: ')
+
+# Get the user's last name.
+last_name = input('Enter your last name: ')
+
+# Print a greeting to the user.
+print('Hello', first_name, last_name)
+
+# Get the user's name, age, and income.
+
+name = input('what is your name? ')
+age = int(input('What is your age? '))
+income = float(input('What is your income? '))
+
+# Display the data
+print('Here is the data you entered:')
+print('Name:', name)
+print('Age', age)
+print('Income', income)
+
+# PRACTICE QUESTION NO.1
+# Get customer's last name
+last_name = 'Gebreamlak'
+
+last_name = input('Enter your last name: ')
+
+# greet customer's with last name
+print('Hello', last_name)
+
+
+# PRACTICE QUESTION NO.2 Program to enter the amount of sales  for the week
+sale_week = float(input('Enter amount of sale for the week '))
